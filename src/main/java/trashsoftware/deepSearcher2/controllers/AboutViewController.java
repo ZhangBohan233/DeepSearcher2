7f630c29d5fc9b1f435a834c751b0810dfe72275
zhangb96 <bokhaam.zhang@mail.utoronto.ca>
2021-01-27 22:57:48 +0800

    Minor updates

--- src/main/java/trashsoftware/deepSearcher2/controllers/AboutViewController.java
+++ src/main/java/trashsoftware/deepSearcher2/controllers/AboutViewController.java
@@ -10,9 +10,12 @@ import java.util.ResourceBundle;
 public class AboutViewController implements Initializable {
     @FXML
     Label authorLabel;
+    @FXML
+    Label versionLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        versionLabel.setText(Client.VERSION);
         if ("CN".equals(resourceBundle.getLocale().getCountry())) {
             authorLabel.setText(Client.AUTHOR_ZH);
         } else {
--- src/main/java/trashsoftware/deepSearcher2/controllers/Client.java
+++ src/main/java/trashsoftware/deepSearcher2/controllers/Client.java
@@ -19,6 +19,8 @@ public class Client extends Application {
     public static final String AUTHOR_ZH = "张博涵";
     public static final String AUTHOR_EN = "Bohan Zhang";
 
+    public static final String VERSION = "1.0";
+
     /**
      * Name of a file which marks the program is running. Create on launch and delete on exit.
      */
